b0aa5b0e04d83f6fbaf397de061b2e670827e5fa
Jeremy Soller <jeremy@system76.com>
2021-02-16 18:24:50 -0700

    Set ME_WE to lock or unlock flash on next reboot

--- src/board/system76/common/security.c
+++ src/board/system76/common/security.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include <board/gpio.h>
 #include <board/security.h>
 
 static enum SecurityState security_state = SECURITY_STATE_LOCK;
@@ -22,9 +23,14 @@ void security_reset(void) {
 
 bool security_set(enum SecurityState state) {
     switch (state) {
-        // Only allow setting the prepare states
+        // Prepare to lock by setting ME_WE low
         case SECURITY_STATE_PREPARE_LOCK:
+            gpio_set(&ME_WE, false);
+            security_state = state;
+            return true;
+        // Prepare to unlock by setting ME_WE high
         case SECURITY_STATE_PREPARE_UNLOCK:
+            gpio_set(&ME_WE, true);
             security_state = state;
             return true;
         // Any other states will be ignored
